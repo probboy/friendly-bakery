7d644fa816a3400c6191ab8a3ea309f2d7d6d775
Warren Tai <warren.w.tai@gmail.com>
2019-01-12 12:38:47 -0500

    MVP

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require "sinatra"
 require_relative "helpers"
 
 get "/" do
-  @page_title = "Cake Shop"
+  @page_title = "Warren's AI Cake Shop"
   @body_id = "home"
   @site_title = "Cake Shop"
   render_view :index
