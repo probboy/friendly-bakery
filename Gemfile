76a0f3793d19a362fabff419af46bc7b85caae91
Warren Tai <warren.w.tai@gmail.com>
2019-01-25 15:30:08 -0500

    remove email validation

--- Gemfile
+++ Gemfile
@@ -2,5 +2,4 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "sendgrid-ruby"
-gem "email_address"
 gem "json"
--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require "sinatra"
 require "sendgrid-ruby"
 include SendGrid
-require "email_address"
 require "json"
 
 require_relative "helpers"
@@ -31,39 +30,35 @@ post "/" do
   @name = params["name"]
   @email = params["email"]
 
-  if EmailAddress.valid? @email
-    from = SendGrid::Email.new(email: "warren.w.tai@gmail.com")
-    to = SendGrid::Email.new(email: @email)
-    subject = "Our Catalogue"
-    content = SendGrid::Content.new(
-      type: "text/plain",
-      value: "Dear " + @name + ",\n
+  from = SendGrid::Email.new(email: "warren.w.tai@gmail.com")
+  to = SendGrid::Email.new(email: @email)
+  subject = "Our Catalogue"
+  content = SendGrid::Content.new(
+    type: "text/plain",
+    value: "Dear " + @name + ",\n
       Attached is the link to the catalogue of our bakery. Enjoy!\n
       Sincerely, \n
       Warren\n
       https://fathomless-island-16806.herokuapp.com/catalogue.pdf",
-    )
+  )
 
-    # create a email object with from, to, subject, and content
-    mail = SendGrid::Mail.new(from, subject, to, content)
+  # create a email object with from, to, subject, and content
+  mail = SendGrid::Mail.new(from, subject, to, content)
 
-    # sets up the API kwy s
+  # sets up the API kwy s
 
-    sg = SendGrid::API.new(
-      api_key: ENV["SENDGRID_API_KEY"],
-    )
+  sg = SendGrid::API.new(
+    api_key: ENV["SENDGRID_API_KEY"],
+  )
 
-    # sends the email
-    response = sg.client.mail._("send").post(request_body: mail.to_json)
-    puts response.status_code
+  # sends the email
+  response = sg.client.mail._("send").post(request_body: mail.to_json)
+  puts response.status_code
 
-    # display http response body
-    puts response.body
+  # display http response body
+  puts response.body
 
-    # display http response headers
-    puts response.headersruby
-    render_view :post
-  else
-    render_view :nopost
-  end
+  # display http response headers
+  puts response.headersruby
+  render_view :post
 end
